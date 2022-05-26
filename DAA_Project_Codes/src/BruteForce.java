097e2fc35b316f0ccc131c8fc94aca8671e058d2
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2022-05-27 01:43:00 +0500

    Initial commit

---
+++ DAA_Project_Codes/src/BruteForce.java
@@ -0,0 +1,72 @@
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
+
+class NQueensBruteForce{
+
+    static String toString(int[] arr){
+        String r = "{ ";
+        for(int i = 0; i < arr.length - 1; i++){
+            r += arr[i] + " , ";
+        }
+        return  r + arr[arr.length - 1] + " }";
+    }
+
+    static int[] copyFromSet(LinkedHashSet<Integer> perm){
+        return perm.stream()
+            .mapToInt(
+                Integer::intValue
+            ).toArray();
+    }
+
+    static void removeLastElement(LinkedHashSet<Integer> perm){
+        Iterator<Integer> i = perm.iterator();
+        for(; i.hasNext(); i.next());
+        i.remove();
+    }
+
+    static void generatePermutations(List <int[]>permList, LinkedHashSet<Integer> perm, int n){
+        if(perm.size() == n) {
+            permList.add(copyFromSet(perm));
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(!perm.contains(i)) {
+                perm.add(i);
+                generatePermutations(permList, perm, n);
+                removeLastElement(perm);
+            }
+        }
+    }
+
+    static boolean isSolution(int []perm){
+        for(int i = 0; i < perm.length - 1; i++) {
+            for(int j = i + 1; j < perm.length; j++){
+                if(Math.abs(i - j) == Math.abs(perm[i] - perm[j])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args){
+        final long startTime = System.currentTimeMillis();
+        int n = 14;
+        
+        var permutations = new LinkedList<int[]>(); // permutations will be saved here
+        
+        generatePermutations(permutations, new LinkedHashSet<Integer>(), n);
+        
+        // for(int []arr : permutations) {
+        //     if(isSolution(arr)) {
+        //         System.out.println(toString(arr)); // print valid solutions.
+        //     }
+        // }
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Total Execution Time: " + (endTime - startTime));
+    }
+
+}
---
+++ DAA_Project_Codes/src/BruteForceWithPruning.java
@@ -0,0 +1,71 @@
+class Queens2 {
+    static int count = 0;
+
+    /***************************************************************************
+     * Prints n-by-n placement of queens in ASCII.
+     ***************************************************************************/
+    public static void printQueens(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i] == j)
+                    System.out.print("Q ");
+                else
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    /***************************************************************************
+     * Solve the n queens problem by brute force.
+     ***************************************************************************/
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    // try all n! permutations, but prune useless ones
+    public static void enumerate(int[] a, boolean[] diag1, boolean[] diag2, int k) {
+        int n = a.length;
+
+        // found one, so print out and stop
+        if (k == 0) {
+            // printQueens(a);
+            // count++;
+            // System.out.println(count);
+            // System.out.println();
+            // // System.exit(0);
+        }
+
+        for (int i = 0; i < k; i++) {
+            swap(a, i, k - 1);
+            int j = k - 1;
+
+            // if placement of new queen is ok, then enumerate
+            if (!diag1[j + a[j]] && !diag2[n + j - a[j]]) {
+                diag1[j + a[j]] = true;
+                diag2[n + j - a[j]] = true;
+                enumerate(a, diag1, diag2, k - 1);
+                diag1[j + a[j]] = false;
+                diag2[n + j - a[j]] = false;
+            }
+            swap(a, i, k - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        final long startTime = System.currentTimeMillis();
+        int n = 16;
+        int[] a = new int[n]; // a[i] = row of queen in ith column
+        boolean[] diag1 = new boolean[2 * n]; // is ith top diagonal occupied?
+        boolean[] diag2 = new boolean[2 * n]; // is ith bottom diagonal occupied?
+        for (int i = 0; i < n; i++)
+            a[i] = i;
+        enumerate(a, diag1, diag2, n);
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Total Execution Time: " + (endTime - startTime));
+    }
+}
