6bb392eaa771b052747487c74841f168cf8e5fb6
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2022-05-27 16:30:26 +0500

    Update Backtracking.java

--- DAA_Project_Codes/src/Backtracking.java
+++ DAA_Project_Codes/src/Backtracking.java
@@ -1,123 +1,68 @@
-/* Java program to solve N Queen Problem using
-backtracking */
-class NQueenProblem {
-    final int N = 4;
 
-    /* A utility function to print solution */
-    void printSolution(int board[][]) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                System.out.print(" " + board[i][j]
-                        + " ");
-            System.out.println();
-        }
-    }
+/* Java program to solve N Queen
+Problem using backtracking */
+import java.util.*;
 
-    /*
-     * A utility function to check if a queen can
-     * be placed on board[row][col]. Note that this
-     * function is called when "col" queens are already
-     * placeed in columns from 0 to col -1. So we need
-     * to check only left side for attacking queens
-     */
-    boolean isSafe(int board[][], int row, int col) {
-        int i, j;
-
-        /* Check this row on left side */
-        for (i = 0; i < col; i++)
-            if (board[row][i] == 1)
-                return false;
-
-        /* Check upper diagonal on left side */
-        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j] == 1)
-                return false;
-
-        /* Check lower diagonal on left side */
-        for (i = row, j = col; j >= 0 && i < N; i++, j--)
-            if (board[i][j] == 1)
-                return false;
-
-        return true;
-    }
-
-    /*
-     * A recursive utility function to solve N
-     * Queen problem
-     */
-    boolean solveNQUtil(int board[][], int col) {
-        /*
-         * base case: If all queens are placed
-         * then return true
-         */
-        if (col >= N)
-            return true;
-
-        /*
-         * Consider this column and try placing
-         * this queen in all rows one by one
-         */
-        for (int i = 0; i < N; i++) {
-            /*
-             * Check if the queen can be placed on
-             * board[i][col]
-             */
-            if (isSafe(board, i, col)) {
-                /* Place this queen in board[i][col] */
-                board[i][col] = 1;
-
-                /* recur to place rest of the queens */
-                if (solveNQUtil(board, col + 1) == true)
-                    return true;
-
-                /*
-                 * If placing queen in board[i][col]
-                 * doesn't lead to a solution then
-                 * remove queen from board[i][col]
-                 */
-                board[i][col] = 0; // BACKTRACK
-            }
-        }
-
-        /*
-         * If the queen can not be placed in any row in
-         * this column col, then return false
-         */
-        return false;
-    }
+class GfG {
 
     /*
      * This function solves the N Queen problem using
-     * Backtracking. It mainly uses solveNQUtil () to
-     * solve the problem. It returns false if queens
-     * cannot be placed, otherwise, return true and
-     * prints placement of queens in the form of 1s.
-     * Please note that there may be more than one
-     * solutions, this function prints one of the
-     * feasible solutions.
+     * Backtracking. It mainly uses solveNQUtil() to
+     * solve the problem.
      */
-    boolean solveNQ() {
-        int board[][] = { { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 } };
+    static List<List<Integer>> nQueen(int n) {
+        // cols[i] = true if there is a queen previously placed at ith column
+        cols = new boolean[n];
+        // leftDiagonal[i] = true if there is a queen previously placed at
+        // i = (row + col )th left diagonal
+        leftDiagonal = new boolean[2 * n];
+        // rightDiagonal[i] = true if there is a queen previously placed at
+        // i = (row - col + n - 1)th rightDiagonal diagonal
+        rightDiagonal = new boolean[2 * n];
+        result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            temp.add(0);
+        solveNQUtil(result, n, 0, temp);
 
-        if (solveNQUtil(board, 0) == false) {
-            System.out.print("Solution does not exist");
-            return false;
-        }
-
-        //printSolution(board);
-        return true;
+        return result;
     }
 
-    // driver program to test above function
-    public static void main(String args[]) {
+    private static void solveNQUtil(List<List<Integer>> result, int n, int row, List<Integer> comb) {
+        if (row == n) {
+            // if row==n it means we have successfully placed all n queens.
+            // hence add current arrangement to our answer
+            // comb represent current combination
+            result.add(new ArrayList<>(comb));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            // if we have a queen previously placed in the current column
+            // or in current left or right diagonal we continue
+            if (cols[col] || leftDiagonal[row + col] || rightDiagonal[row - col + n])
+                continue;
+            // otherwise we place a queen at cell[row][col] and
+            // make current column, left diagonal and right diagonal true
+            cols[col] = leftDiagonal[row + col] = rightDiagonal[row - col + n] = true;
+            comb.set(col, row + 1);
+            // then we goto next row
+            solveNQUtil(result, n, row + 1, comb);
+            // then we backtrack and remove our currently placed queen
+            cols[col] = leftDiagonal[row + col] = rightDiagonal[row - col + n] = false;
+        }
+    }
+
+    static List<List<Integer>> result = new ArrayList<List<Integer>>();
+    static boolean[] cols, leftDiagonal, rightDiagonal;
+
+    // Driver code
+    public static void main(String[] args) {
         final long startTime = System.currentTimeMillis();
-        NQueenProblem Queen = new NQueenProblem();
-        Queen.solveNQ();
+        int n = 16;
+
+        List<List<Integer>> res = nQueen(n);
+        // System.out.println(res);
         final long endTime = System.currentTimeMillis();
         System.out.println("Total Execution Time: " + (endTime - startTime));
     }
 }
-// This code is contributed by Abhishek Shankhadhar
